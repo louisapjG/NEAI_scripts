3cd58d1acee543e992a00c17ebd887b990bbb783
Louis <louis.gobin08@gmail.com>
2021-08-05 15:37:06 -0700

    change in progress -- non viable

--- run_scripts/data_org_run.py
+++ run_scripts/data_org_run.py
@@ -13,6 +13,9 @@ import numpy as np
 #NEAI_CLI_path = '/home/louis/Desktop/neai_cli_unix_v2021.05.03.2/neai_cli'
 NEAI_CLI_path = '/home/ludusmagnus/Desktop/neai_cli_unix_v2021.05.14.1/neai_cli'
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*********How to select all Ys and then the individual combinations. Also name column in wrting to CSV*********
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 def data_org_run(args, files_in_opt=[]):
 	if files_in_opt != []:
@@ -200,6 +203,148 @@ def NEAI_AD(train_files,nbr_axis,root_name,MCU,RAM,value_delimiter,search_time,s
 
 	return results_dic
 
+def NEAI_clf(train_files,nbr_axis,root_name,MCU,RAM,value_delimiter,search_time,signal_length,freq):
+	algo_type = 'classification'
+	project_Name = root_name+'-'+str(signal_length)+'-F'+str(freq)
+	
+	#Start NEAI engine
+	result = subprocess.Popen([NEAI_CLI_path,#str('.'+NEAI_CLI_path[NEAI_CLI_path.rfind('/'):]),
+								'engine',
+								'-launch',
+								],cwd=NEAI_CLI_path[:NEAI_CLI_path.rfind('/')],stdout=subprocess.PIPE)
+	#print(result)
+	
+	time.sleep(int(21))
+	#print('launch')
+	result = subprocess.run([NEAI_CLI_path,
+							'engine',
+							'-test',
+							],stdout=subprocess.PIPE)
+	#print("Post launch engine check: ",result.stdout,'\n')
+
+	#Display settings
+	result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+							'display',
+							'-print','-no_pretty_print',
+							],stdout=subprocess.PIPE)
+
+	#print('display settings change')
+	#Create project
+	result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+							'project',
+							'-create',
+							str('-'+algo_type),
+							'-slug_name',project_Name,
+							'-mcu',MCU,
+							'-max_ram',str(RAM),
+							'-number_axis',str(nbr_axis),
+							],stdout=subprocess.PIPE)
+	#print("project creation: ",result.stdout)
+	#print()
+
+	signals_used = ''
+	signal_nbr = 1
+	for file in train_files:
+		signal_id = str(signal_nbr)
+		result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+								'signal',
+								'-import',
+								'-file_path',file,
+								'-class', '-name', os.path.basename(file),
+								'-delimiter',value_delimiter,
+								'-id', signal_id,
+								'-project',project_Name,
+								],stdout=subprocess.PIPE)
+
+		#time.sleep(100)
+		if signal_nbr == 1: signals_used = signal_id
+		else: signals_used = signals_used + ',' + signal_id
+		signal_nbr = signal_nbr + 1
+		#print("Signal import",result)
+		#print(signal_nbr, file)
+	#Launch optimization
+	#print(signals_used)
+	result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+							'optimization',
+							'-launch',
+							str('-signals='+signals_used),
+							'-project',project_Name,
+							'-nb_cores', str(22),
+							],stdout=subprocess.PIPE)
+	#print('Launch: ',json.loads(result.stdout))
+	#time.sleep(5)
+	opt_id = int(json.loads(result.stdout)['optimization']['id'])
+	#print()
+
+	iteration = 0
+	progress = 0
+	while (iteration < search_time/15) and progress < 1:
+		time.sleep(int(10))
+		result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+								'optimization',
+								'-progress',
+								'-id', str(opt_id),
+								'-project',project_Name,
+								],stdout=subprocess.PIPE)
+		try:
+			progress = float(json.loads(result.stdout)['progress'])
+		except Exception as e:
+			print("Search status: ",result.stdout)
+		iteration += 1
+
+	#Stop optimization
+	result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+							'optimization',
+							'-stop',
+							],stdout=subprocess.PIPE)
+	#print(result.stdout)#display_results(result)
+
+	#Get results
+	result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+							'optimization',
+							'-result',
+							'-id',str(opt_id),
+							'-project',project_Name,
+							],stdout=subprocess.PIPE)
+	results_dic = json.loads(result.stdout)
+	#print('Precision',results_dic['precision'])
+	#print('Confidence',results_dic['confidence'])
+	#print('RAM',results_dic['estimated_ram'])
+
+	#Delete project
+	# result = subprocess.run([NEAI_CLI_path,#str('.'+NEAI_CLI_path),
+	# 						'project',
+	# 						'-delete',
+	# 						'-slug_name',args.project_Name,
+	# 						],stdout=subprocess.PIPE)
+	#print(result.stdout)#display_results(result)
+
+	#Stop NEAI engine
+	result = subprocess.run([NEAI_CLI_path,
+							'engine',
+							'-shutdown',
+							],stdout=subprocess.PIPE)
+	time.sleep(int(5))
+
+	return results_dic
+
+#TO MAKEWORK
+def Ys_gen():
+	#Organize files all in one
+	nominal_files = []
+	for fold in noms:
+		nom_path = os.path.join(nom_path_root,fold)
+		for f in os.listdir(nom_path):
+			if os.path.isfile(os.path.join(nom_path, f)):
+				nominal_files.append(os.path.join(nom_path, f))
+	
+	abnormal_files = []
+	for fold in abns:
+		abn_path = os.path.join(abn_path_root,fold)
+		for f in os.listdir(abn_path): 
+			if os.path.isfile(os.path.join(abn_path, f)):
+				abnormal_files.append(os.path.join(abn_path, f))
+
 def main():
 	result_path ='report_Paderborne_AD_Y12.csv'
 
@@ -210,30 +355,16 @@ def main():
 	args = read_args()
 
 	with open(result_path, 'a') as f:
-		f.write('Y,Buffer_length,Subsampling,Precision,Confidence,Source_file_nom,Source_file_abn\n')
+		f.write('Y,Y_subselection,Buffer_length,Subsampling,Precision,Confidence,Source_file_nom,Source_file_abn\n')
 
 	# for noms in noms_:
 	# 	noms = [noms]
 
 	for Y in ['Y12','Y3']:
 
+		#Data source
 		nom_path_root = '/media/ludusmagnus/b3008e59-6246-4c41-a0ce-b9090b4d8f99/paderborn_csv/'+Y+'/'#'/home/ludusmagnus/Desktop/datasets/paderborn_csv/'+Y+'/'
 		abn_path_root = '/media/ludusmagnus/b3008e59-6246-4c41-a0ce-b9090b4d8f99/paderborn_csv/'+Y+'/'#'/home/ludusmagnus/Desktop/datasets/paderborn_csv/'+Y+'/'
-
-		nominal_files = []
-		for fold in noms:
-			nom_path = os.path.join(nom_path_root,fold)
-			for f in os.listdir(nom_path):
-				if os.path.isfile(os.path.join(nom_path, f)):
-					nominal_files.append(os.path.join(nom_path, f))
-		
-		abnormal_files = []
-		for fold in abns:
-			abn_path = os.path.join(abn_path_root,fold)
-			for f in os.listdir(abn_path): 
-				if os.path.isfile(os.path.join(abn_path, f)):
-					abnormal_files.append(os.path.join(abn_path, f)) 
-
 
 		root_name = 'JD-AUTO-ad-'+Y
 		args.nbr_axis = 1
@@ -249,8 +380,15 @@ def main():
 		RAM = 128000
 		search_time = 3600
 
-		for freq in freqs:
-			for signal_length in signal_lengths:
+		# for freq in freqs:
+		# 	for signal_length in signal_lengths:
+		for freq, signal_length in  freqs_signL:
+
+			for Yc in range(len(noms)+len(abns)+1):
+				####
+				####
+				###
+
 				print(Y,noms[0][:-1],freq,signal_length)
 
 				args.buffer_sizes = [signal_length]
@@ -282,7 +420,7 @@ def main():
 				
 				with open(result_path, 'a') as f:
 					#'Y,Buffer_length,Subsampling,Precision,Confidence,Source_file_nom,Source_file_abn'
-					f.write(str(Y+','+str(signal_length)+','+str(freq)+','+str(results_dic['precision'])+','+str(results_dic['confidence'])+','+file_nominal[0]+','+file_abnormal[0]+'\n'))
+					f.write(str(Y+','+''+','+str(signal_length)+','+str(freq)+','+str(results_dic['precision'])+','+str(results_dic['confidence'])+','+file_nominal[0]+','+file_abnormal[0]+'\n'))
 
 		#
 
